26179fb61ffe0a2a3b83ea8ac2f09e8469a84b71
binja <buzzzzx233@gmail.com>
2017-10-23 11:12:59 +0800

    finish 0000

---
+++ 0000/avatar_add_number.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+__author__ = 'buzz'
+__date__ = '2017/10/23 上午10:12'
+
+"""
+将你的 QQ 头像（或者微博头像）右上角加上红色的数字，类似于微信未读信息数量那种提示效果。
+Pillow：Python Imaging Library
+PIL.ImageDraw.Draw.text(xy, text, fill=None, font=None, anchor=None)
+"""
+
+from PIL import Image, ImageDraw, ImageFont
+
+avatar = "weChat_avatar.png"
+the_font = "arial.ttf"
+color = (255, 0, 0)
+
+
+def draw_text(number, color, the_font):
+    img = Image.open(avatar)
+    x, y = img.size
+    print("The image's size is:")
+    print(img.format, img.size, img.mode)
+
+    draw = ImageDraw.Draw(img)
+    font = ImageFont.truetype(the_font, 35)
+    draw.text((x-20, 0), number, fill=color, font=font)
+    img.show()
+    img.save("new_avatar.png")
+
+
+if __name__ == '__main__':
+    number = str(7)
+    draw_text(number, color, the_font)
